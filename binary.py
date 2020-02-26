64acbd64224bdf8fe423edc1cc23d953e9b4eeb8
Baabu320 <61137758+Baabu320@users.noreply.github.com>
2020-02-26 23:36:59 +0100

    Add files via upload
    #python
    #calendar
    #date
    #time
    #beginerlevel
    #learner
    #python journey

---
+++ binary.py
@@ -0,0 +1,59 @@
+a=10
+i=1
+i=i+1
+print(i)
+
+b=3
+b=b+4
+print(b)
+
+x=6
+x-=2
+print (x)
+
+import time
+
+#print the number of ticks spent since 12 AM, 1st January 2020
+
+print(time.time())
+
+#print the number of ticks spent since 12 AM, 25th April 1990
+
+print(time.time())
+
+#returns a time tuple
+
+print(time.localtime())
+
+
+import datetime;
+
+#return the current datetime object
+
+print(datetime.datetime.now())
+
+import calendar;
+
+cal= calendar.month(2020,12)
+
+#print the calendar of December 2020
+
+print(cal)
+
+cal= calendar.month(2020,2)
+
+#print the calendar of February 2020
+
+print(cal)
+
+cal= calendar.month(2020,5)
+
+#print the calendar of may 2020
+
+print(cal)
+
+#printing the calendar of the year 2019
+calendar.prcal(2019)
+
+#printing the calendar of the year 2019
+calendar.prcal(2020)
